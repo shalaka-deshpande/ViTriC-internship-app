8b5a72b10fa93ccb231d6e5d3b3807ea3fcdb216
Shalaka <shalaka_deshpande@outlook.com>
2025-01-05 21:15:24 +0530

    Refactored card-user component

--- src/app/components/card-user.tsx
+++ src/app/components/card-user.tsx
@@ -20,7 +20,7 @@ export default function UserInformation(){
                 <div className=" bg-slate-300/50 p-4 mx-3 rounded-lg">
                     Name: First Middle Last
                 </div>
-                <div className="bg-pink-400"> </div>
+                <div className="bg-pink-400"></div>
                 <div className="bg-pink-500"></div>
                 <div className="bg-pink-600"></div>
                 <div className="bg-pink-700"></div>
