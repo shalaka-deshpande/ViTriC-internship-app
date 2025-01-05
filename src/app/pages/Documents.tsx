c94e29f766c246a9a08663ce09219c524c20de37
Shalaka <shalaka_deshpande@outlook.com>
2025-01-05 17:05:55 +0530

    Added placeholder page layout

--- src/app/pages/Documents.tsx
+++ src/app/pages/Documents.tsx
@@ -1,8 +1,14 @@
-
+import Page from "../layout"
 export default function Documents() {
     return (
-        <div>
-            Hello Documents
+        <Page>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-2">
+            <div className="aspect-video rounded-xl bg-blue-200/50" />
+            <div className="aspect-video rounded-xl bg-blue-200/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-pink-400/50 md:min-h-min" />
         </div>
+        </Page>
     )
 }
---
+++ src/app/pages/Strong-Skills.tsx
@@ -0,0 +1,14 @@
+import Page from "../layout"
+export default function StrongSkills() {
+    return (
+        <Page>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-2">
+            <div className="aspect-video rounded-xl bg-green-700/50" />
+            <div className="aspect-video rounded-xl bg-green-700/50" />
+          </div>
+          <div className="min-h-[100vh] flex-1 rounded-xl bg-cyan-400/50 md:min-h-min" />
+        </div>
+        </Page>
+    )
+}
